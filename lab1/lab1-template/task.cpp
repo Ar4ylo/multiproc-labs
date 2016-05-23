da0baada54911edbeae827979039d724d9d82c42
MiRuusskikh <tadjik.ds@gmail.com>
2016-05-23 09:54:30 +0300

    Revert "1"
    This reverts commit 8a4ac80c3ce6bc0ac36d41c0cc3de56da42e107e.

--- lab1/lab1-template/task.cpp
+++ lab1/lab1-template/task.cpp
@@ -1,67 +1,8 @@
 
 #include "stdafx.h"
-<<<<<<< HEAD
-#include "stdio.h"
-#include "iostream"
-#include "clocale"
-#include "omp.h"
-using namespace std;
 
-int performArrayTask(long size)
-{
-	setlocale(LC_ALL, "Rus");
-	long S;
-	S=0;
-	long *arr = new long[size];
-
-	for(long i=0; i<size; i++)
-		{
-			arr[i] = i;		
-		}
-
-	double start_time=omp_get_wtime();
-	
-	#pragma omp parallel num_threads(4)
-	{
-		long local_sum = 0;
-		#pragma omp for
-		for(long i=0; i<size; i++)
-		{
-			local_sum+=arr[i];
-		}
-		S+=local_sum;
-	}
-	delete []arr;
-	double end_time=omp_get_wtime();
-
-cout<<"Сумма элементов массива: "<<(unsigned long)S;
-printf("\n замер времени %lf\n", end_time-start_time);
-
-return 0;
-	
-}
-
-=======
-#include <stdio.h>
-#include "iostream"
-#include <omp.h>
-
-using namespace std;
 int performArrayTask(int size)
-{    
-	setlocale(LC_ALL);
-	long s;
-	s = 0;
-	long *arr = new long[size];
-
-	for(long i=0; i<size; i++)
-    #pragma omp for
-	{
-		arr[i] = i;
-		s=s+arr[i];
-	}
-	delete []arr;
-	double end_time=omp_get_wtime;
+{
+	//TODO: add code here
 	return 0;
 }
->>>>>>> origin/master
